b95ced6eaf55f716149cc2dc01a29f93798fe325
RaiYunan <yunanrai433@gmail.com>
2025-07-20 10:58:30 +0545

    Frontend setup-Tailwind and ShadCN UI

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # MERN-BLOG
 🌐 A full-featured Blog Web Application built with the MERN stack (MongoDB, Express, React, Node.js) and styled using Tailwind CSS. Features include user authentication, blog creation/editing, and responsive design.
+
+# 📝 Full Stack Blog Application (MERN + Tailwind)
+
+A complete blog application built with the **MERN stack** (MongoDB, Express, React, Node.js) and styled using **Tailwind CSS**. This project includes full CRUD functionality, authentication, and a responsive design.
+
+---
+
+## 🚀 Features
+
+- 🧑‍💻 User Authentication (JWT-based)
+- 📝 Create, Edit, Delete Blogs
+- 🔍 View all posts with pagination
+- 📁 Upload and display images
+- 🌓 Light/Dark Theme Toggle
+- 📱 Fully Responsive UI (Tailwind CSS)
+- 🔒 Protected Routes
+
+---
+
+## 📦 Tech Stack
+
+**Frontend:**
+- React
+- React Router
+- Axios
+- Tailwind CSS
+
+**Backend:**
+- Node.js
+- Express.js
+- MongoDB + Mongoose
+- JSON Web Token (JWT)
+- Multer (for image uploads)
+
+---
+
+## ⚙️ Installation
+
+### 1. Clone the repo
+
+```bash
+git clone https://github.com/your-username/mern-blog-app.git
+cd mern-blog-app
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from './components/ui/button'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full min-h-screen bg-white text-black text-xl font-bold flex justify-center items-center gap-2'>
+        BlogMe
+        <Button className="bg-green-700 hover:bg-green-900 duration-700 text-xl">
+        Click Me!
+      </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
