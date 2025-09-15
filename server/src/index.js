790cf0c66b1c2383dcc5aa162a3d89ffa06a6d0c
RaiYunan <yunanrai433@gmail.com>
2025-09-15 11:33:14 +0545

    Backend setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,38 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import {name} from "../../client/src/assets/sample.js"
+
 dotenv.config({
     path:"./.env"
 });
-const app=express();
 
+const app=express();
 const port=process.env.PORT
 
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
+app.use(cookieParser());
+
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+ res.json({
+    name:"Yunan Rai",
+    age:18,
+    crush:name
+ })
+});
+
 app.get("/api/data",(req,res)=>{
     res.json({message:`I like ${name}`});
 })
