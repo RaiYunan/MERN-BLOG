55ac3ef30cb06fc7a22640c8f27d97b4a8f39ab2
RaiYunan <yunanrai433@gmail.com>
2025-09-28 09:17:58 +0545

    Redux-persist setup

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from "../redux/user/user.slice.js"
+import { persistStore, persistReducer } from 'redux-persist'
+import sessionStorage from 'redux-persist/es/storage/session'
+
+const rootReducer=combineReducers({
+  user:userReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage:sessionStorage,
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer)
 
 //useSelector=used to access current state of store
 //useDispatch=used to update the store by sending payload(data)
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
+  reducer: persistedReducer,
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware({serializableCheck:false})
 })
+
+export const persister=persistStore(store)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { Provider } from 'react-redux'
-import { store } from './app/store.js'
+import { persister, store } from './app/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persister}>
          <App />
        <ToastContainer/>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
