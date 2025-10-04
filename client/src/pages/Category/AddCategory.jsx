edfb315f06e45e7314ddac6e20d2f35a78da38cc
RaiYunan <yunanrai433@gmail.com>
2025-10-04 21:33:04 +0545

    Added slugify package and implemented  dynamic slug generation in AddCategory form

--- client/src/pages/Category/AddCategory.jsx
+++ client/src/pages/Category/AddCategory.jsx
@@ -1,6 +1,6 @@
-import { Card } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate,Link } from 'react-router-dom';
@@ -17,6 +17,7 @@ import { Input } from "@/components/ui/input";
 import { RouteIndex, RouteSignIn } from "@/helpers/RouteName";
 import { showToast } from "@/helpers/showToast";
 import { Button } from "@/components/ui/button";
+import slugify from 'slugify';
 
 const AddCategory = () => {
      const dispatch=useDispatch()
@@ -34,53 +35,32 @@ const AddCategory = () => {
         },
       });
     
+      const nameValue=form.watch("name")
+
+      useEffect(()=>{
+        if(nameValue.trim()){
+            const generatedSlug=slugify(nameValue,{
+                trim:true,
+                strict:true,//removes ALL special characters except letters, numbers, and hyphens.
+                lower:true
+            })
+            form.setValue("slug",generatedSlug)
+        }
+
+      },[nameValue,form])
+    
       async function onSubmit(values) {
-        // try {
-        //   const url=`${import.meta.env.VITE_URL}/auth/login`
-        //   const response=await fetch(url,
-        //     {
-        //       method:"POST",
-        //       headers:{"Content-type":"application/json"},
-        //       credentials:"include",
-        //       body:JSON.stringify(values),
-        //     }
-        //   )
-    
-        //   const data=await response.json();
-    
-        //   console.log(response);
-        //   if(!response.ok){
-        //     console.log("Error while logging in",data)
-        //     showToast("error",data.message);
-        //     return;
-        //   }
-        //   console.log("before dispatch",data)
-        //   dispatch(setUser(data))
-        //   navigate(RouteIndex);
-        //   showToast("success","User logged in sucessfully")
-          
-        // } catch (error) {
-        //   showToast("error",error.message)
-        // }
+        console.log(values);
       }
     
   return (
-    <div className="flex w-full items-center justify-center">
-      <Card className="w-[400px] p-5 ">
-        <h1 className="text-2xl font-bold text-center">
-          Add Category
-        </h1>
-        <div className=''>
-          <div className='border flex justify-center items-center mt-5'>
-            <span className='absolute bg-white text-sm '>Or</span>
-          </div>
-        </div>
-
-
-        <Form {...form}>
+    <div className="pt-5 max-w-screen-md mx-auto">
+      <Card>
+        <CardContent>
+             <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
 
-              <div className="mb-3">
+              <div className="mb-5 px-5">
               <FormField
                 control={form.control}
                 name="name"
@@ -96,7 +76,7 @@ const AddCategory = () => {
               />
             </div>
 
-            <div className="mb-3">
+            <div className="mb-5 px-5">
               <FormField
                 control={form.control}
                 name="slug"
@@ -120,6 +100,9 @@ const AddCategory = () => {
              </Button>
           </form>
         </Form>
+
+        </CardContent>
+       
       </Card>
     </div>
   )
