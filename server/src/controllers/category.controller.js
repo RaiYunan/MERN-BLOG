0f107a1a6f2c01fc62f2482661ae9a78b215b3f4
RaiYunan <yunanrai433@gmail.com>
2025-10-05 18:59:34 +0545

    Create Redux slice for category state, Fix various UI bugs and layout issues,Add category display components

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -3,29 +3,37 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 
-export const addCategory=asyncHandler(async(req,res,next)=>{
-    const {name,slug}=req.body
+export const addCategory = asyncHandler(async (req, res, next) => {
+  const { name, slug } = req.body;
 
-    const category=await Category.create({
-        name:name,
-        slug:slug
-    })
+  const category = await Category.create({
+    name: name,
+    slug: slug,
+  });
 
-    if(!category){
-        throw new ApiError(400,"Something went wrong while adding Category")
-    }
-    console.log("Category saved",category)
-    res.status(200).json(new ApiResponse(200,category,"Category added successfully"));
-})
+  if (!category) {
+    throw new ApiError(400, "Something went wrong while adding Category");
+  }
+  console.log("Category saved", category);
+  res
+    .status(200)
+    .json(new ApiResponse(200, category, "Category added successfully"));
+});
 
-export const showCategory=asyncHandler(async(req,res,next)=>{
-    
-})
+export const showCategory = asyncHandler(async (req, res, next) => {});
 
-export const editCategory=asyncHandler(async(req,res,next)=>{
-    
-})
+export const editCategory = asyncHandler(async (req, res, next) => {});
 
-export const deleteCategory=asyncHandler(async(req,res,next)=>{
-    
-})
+export const deleteCategory = asyncHandler(async (req, res, next) => {});
+
+export const getAllCategories = asyncHandler(async (req, res, next) => {
+  //Fetches all categories from the database.
+  //Returns: Newest documents first (most recent at top)
+  //Returns them as plain JavaScript objects (not Mongoose documents).
+  // Waits for the query to complete and returns the result.
+  const categories = await Category.find().sort({ name: -1 }).lean().exec();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, categories, "All categories fetched successfully."));
+});
--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { addCategory } from "../controllers/category.controller.js";
+import { addCategory, deleteCategory, editCategory, getAllCategories, showCategory } from "../controllers/category.controller.js";
 
 const router=Router()
 
 router.route("/add-category").post(addCategory);
-
+router.route("/edit-category/:categoryId").post(editCategory);
+router.route("/show-category/:categoryId").get(showCategory);
+router.route("/delete-category/:categoryId").post(deleteCategory);
+router.route("/all-category").get(getAllCategories);
 export default router
