6ad30883845bf5e7923adbe143a0c2da5106ebf9
RaiYunan <yunanrai433@gmail.com>
2025-10-28 23:59:03 +0545

    feat: Successfully integrated frontend and backend to get all comments and all comments data is shown through shadcn table component

--- client/src/pages/CommentsList.jsx
+++ client/src/pages/CommentsList.jsx
@@ -17,8 +17,24 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useFetch } from "@/hooks/useFetch";
+import Loading from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import { MdDelete } from "react-icons/md";
 
 const CommentsList = () => {
+  const url = `${import.meta.env.VITE_URL}/blog/comments`;
+  const {
+    data: commentsData,
+    loading,
+    error,
+  } = useFetch(url, {
+    method: "GET",
+    credentials: "include",
+  });
+
+  console.log("Comments Data;-\n", commentsData);
+  if (loading) return <Loading />;
   return (
     <div>
       <Card>
@@ -27,7 +43,8 @@ const CommentsList = () => {
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[250px]">Blog Post</TableHead>
+                <TableHead className="w-[350px]">Blog Post</TableHead>
+                <TableHead className="w-[150px]">Category</TableHead>
                 <TableHead className="w-[120px]">Comment By</TableHead>
                 <TableHead className="w-[100px]">Date Posted</TableHead>
                 <TableHead className="w-[180px]">Comment Content</TableHead>
@@ -36,14 +53,31 @@ const CommentsList = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">INV001</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>Credit Card</TableCell>
-                <TableCell className="">$250.00</TableCell>
-                <TableCell className="">$250.00</TableCell>
-                <TableCell className="">$250.00</TableCell>
-              </TableRow>
+              {commentsData && commentsData.length > 0
+                ? commentsData.map((comment) => {
+                    return (
+                    
+                        <TableRow key={comment._id}>
+                          <TableCell className="max-w-[350px] whitespace-normal break-words">{comment.blogId.title}</TableCell>
+                          <TableCell className="w-[150px]">{comment.blogId.category.name}</TableCell>
+                          <TableCell className="w-[120px]">{comment.author.name}</TableCell>
+                          <TableCell className="w-[100px]">{new Date(comment.createdAt).toLocaleDateString("en-US",{
+                            year:"numeric",
+                            month:"short",
+                            day:"numeric"
+                          })}</TableCell>
+                          <TableCell className="max-w-[180px] whitespace-normal break-words">{comment.content}</TableCell>
+                          <TableCell className="w-[80px] text-center">{comment.status}</TableCell>
+                          <TableCell className="w-[120px] text-center">
+                            <Button variant="destructive" className="cursor-pointer">
+                                <MdDelete />
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                
+                    );
+                  })
+                : "No comments available"}
             </TableBody>
           </Table>
         </CardContent>
