67c81470025a369e9644947146b2f91526fb1f32
RaiYunan <yunanrai433@gmail.com>
2025-10-29 23:55:40 +0545

    feat:add dialog box to delete comments made by user(functionality to be implememted soon)

--- client/src/pages/CommentsList.jsx
+++ client/src/pages/CommentsList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardAction,
@@ -21,8 +21,21 @@ import { useFetch } from "@/hooks/useFetch";
 import Loading from "@/components/Loading";
 import { Button } from "@/components/ui/button";
 import { MdDelete } from "react-icons/md";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
 
 const CommentsList = () => {
+  const [refreshData, setRefreshData] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+  const [open, setOpen] = useState(false);
+
   const url = `${import.meta.env.VITE_URL}/blog/comments`;
   const {
     data: commentsData,
@@ -33,6 +46,53 @@ const CommentsList = () => {
     credentials: "include",
   });
 
+    const ConfirmDialog = ({ open, onClose, onConfirm, title, description }) => {
+    return (
+      <Dialog open={open} onOpenChange={onClose}>
+        <DialogContent className="sm:max-w-[400px] px-10 py-12 flex-col gap-6">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{description}</DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter className="flex justify-end gap-2">
+            <Button
+              variant="outline"
+              onClick={onClose}
+              className="cursor-pointer"
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={onConfirm}
+              className="cursor-pointer"
+            >
+              Confirm
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  const openDeleteDialog = (commentId) => {
+    console.log("Button clicked.")
+    setCommentToDelete(commentId);
+    setOpen(true);
+  };
+
+  const closeDeleteDialog = () => {
+    setOpen(false);
+    setCommentToDelete(null);
+  };
+
+  const handleDelete = async () => {
+    if (!commentToDelete) {
+      return;
+    }
+    console.log("gula")
+  };
   console.log("Comments Data;-\n", commentsData);
   if (loading) return <Loading />;
   return (
@@ -56,25 +116,42 @@ const CommentsList = () => {
               {commentsData && commentsData.length > 0
                 ? commentsData.map((comment) => {
                     return (
-                    
-                        <TableRow key={comment._id}>
-                          <TableCell className="max-w-[350px] whitespace-normal break-words">{comment.blogId.title}</TableCell>
-                          <TableCell className="w-[150px]">{comment.blogId.category.name}</TableCell>
-                          <TableCell className="w-[120px]">{comment.author.name}</TableCell>
-                          <TableCell className="w-[100px]">{new Date(comment.createdAt).toLocaleDateString("en-US",{
-                            year:"numeric",
-                            month:"short",
-                            day:"numeric"
-                          })}</TableCell>
-                          <TableCell className="max-w-[180px] whitespace-normal break-words">{comment.content}</TableCell>
-                          <TableCell className="w-[80px] text-center">{comment.status}</TableCell>
-                          <TableCell className="w-[120px] text-center">
-                            <Button variant="destructive" className="cursor-pointer">
-                                <MdDelete />
-                            </Button>
-                          </TableCell>
-                        </TableRow>
-                
+                      <TableRow key={comment._id}>
+                        <TableCell className="max-w-[350px] whitespace-normal break-words">
+                          {comment.blogId.title}
+                        </TableCell>
+                        <TableCell className="w-[150px]">
+                          {comment.blogId.category.name}
+                        </TableCell>
+                        <TableCell className="w-[120px]">
+                          {comment.author.name}
+                        </TableCell>
+                        <TableCell className="w-[100px]">
+                          {new Date(comment.createdAt).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </TableCell>
+                        <TableCell className="max-w-[180px] whitespace-normal break-words">
+                          {comment.content}
+                        </TableCell>
+                        <TableCell className="w-[80px] text-center">
+                          {comment.status}
+                        </TableCell>
+                        <TableCell className="w-[120px] text-center">
+                          <Button
+                            variant="destructive"
+                            className="cursor-pointer"
+                            onClick={()=>openDeleteDialog(comment._id)}
+                          >
+                            <MdDelete  />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
                     );
                   })
                 : "No comments available"}
@@ -82,6 +159,14 @@ const CommentsList = () => {
           </Table>
         </CardContent>
       </Card>
+
+      <ConfirmDialog
+        open={open}
+        onClose={closeDeleteDialog}
+        onConfirm={handleDelete}
+        title="Delete this comment?"
+        description="This action cannot be undone. The blog will be permanently deleted."
+      />
     </div>
   );
 };
